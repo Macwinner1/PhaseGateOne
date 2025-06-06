08b0980e6bf4c3c24bf08e948200969240333cd0
Macwinner1 <ifechukwuw@gmail.com>
2025-06-06 19:50:20 +0100

    refactoring my function

--- Python file/menstrualcycleapp.py
+++ Python file/menstrualcycleapp.py
@@ -1,37 +1,39 @@
 from datetime import datetime, timedelta 
 
-
-cycle_length = 1
+cycle_length = 0
 months = 0
 estimate_ovulation = 0
 fertile_days = 0
 
 def valid_year(year):
-	if year > 2025 or year < 2025:
-		return "Invalid year"	
 	return year
 
 def valid_month(month):
-	if month > 12 or month < 1:
-		return "Invalid month"
 	return month 
 
 def valid_day(day):
-	if day > 31 or day < 1:
-		return "Invalid day"
 	return day 
 
-def first_day(year, month, day):
-	start_date = datetime(year, month, day)
+def date_validation(date):
+	year, month, day = map(int, date.replace(",", "").split())
+	if year > 2025 or year < 2025:
+		return "Invalid year"	
+	elif month > 12 or month < 1:
+		return "Invalid month"
+	elif day > 31 or day < 1:
+		return "Invalid day"
+	else:
+		date = datetime(year, month, day)
+	return date
+
+
+def first_day(date):
+	start_date = date_validation(date)
 	return start_date
 	
-def last_day(year, month, day):
-	end_date = datetime(year, month, day)
-	return end_date
-
-def calculate_cycle_length(start_date, end_date):
+def calculate_cycle_length(start_date):
 	global cycle_length
-	cycle_length = (start_date - end_date).days
+	cycle_length = start_date
 	return cycle_length
 
 
@@ -50,13 +52,12 @@ while exit:
 	menu_input = input()
 	match menu_input:
 		case '1':
-			#global cycle_length
-			print("Enter Last Date Period Started (first_day(year, month, day)): ")
-			start_date = int(input())
+			print("Enter Last Date Period Started (eg. 2025, 1, 5) : ")
+			start_date = first_day(input())
 			print("Enter Cycle length (1 - 35):")
-			cycle_length = int(input())
-			next_period_date = int(start_date + timedelta(days=cycle_length))
-			ovulation_day = int(next_period_date - timedelta(days=14))
+			calculate_cycle_length(int(input()))
+			next_period_date = start_date + timedelta(days= cycle_length)
+			ovulation_day = next_period_date - timedelta(days=14)
 			fertile_window_start = ovulation_day - timedelta(days=5)
 			fertile_window_end = ovulation_day + timedelta(days=1)
 			print(next_period_date)
@@ -66,6 +67,5 @@ while exit:
 						
 		case '0':
 			exit = False
-
 
 
