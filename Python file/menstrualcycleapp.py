4abecf86d9c580d3bec01f3b58ce85b5ac93c00f
Macwinner1 <ifechukwuw@gmail.com>
2025-06-07 06:03:06 +0100

    still refactoring my code

--- Python file/menstrualcycleapp.py
+++ Python file/menstrualcycleapp.py
@@ -1,18 +1,6 @@
 from datetime import datetime, timedelta 
 
 cycle_length = 0
-months = 0
-estimate_ovulation = 0
-fertile_days = 0
-
-def valid_year(year):
-	return year
-
-def valid_month(month):
-	return month 
-
-def valid_day(day):
-	return day 
 
 def date_validation(date):
 	year, month, day = map(int, date.replace(",", "").split())
@@ -44,6 +32,7 @@ menu = '''
 	Press:
 	1. Check your Menstrual Cycle
 	0. Exit
+===========================================
 	'''
 	
 exit = True
@@ -52,18 +41,50 @@ while exit:
 	menu_input = input()
 	match menu_input:
 		case '1':
-			print("Enter Last Date Period Started (eg. 2025, 1, 5) : ")
-			start_date = first_day(input())
-			print("Enter Cycle length (1 - 35):")
-			calculate_cycle_length(int(input()))
-			next_period_date = start_date + timedelta(days= cycle_length)
-			ovulation_day = next_period_date - timedelta(days=14)
-			fertile_window_start = ovulation_day - timedelta(days=5)
-			fertile_window_end = ovulation_day + timedelta(days=1)
-			print(next_period_date)
-			print(ovulation_day)
-			print(fertile_window_start)
-			print(fertile_window_end)
+			inner_menu = '''
+===========================================
+	Menstrual Cycle Result Menu
+	
+	Press:
+	1. View all the Result
+	2. View Your Next Period Date
+	3. View Your Ovulation Date
+	4. View Your Safe Day
+	5. View Your Fertile Window
+	0. <= Back
+	
+===========================================
+			
+					'''
+			exit1 = True
+			while exit1:
+				print("Enter Last Date Period Started (eg. 2025, 1, 5) : ")
+				start_date = first_day(input())
+				print("Enter Cycle length (1 - 35):")
+				calculate_cycle_length(int(input()))
+				next_period_date = start_date + timedelta(days= cycle_length)
+				ovulation_day = next_period_date - timedelta(days=14)
+				fertile_window_start = ovulation_day - timedelta(days=5)
+				fertile_window_end = ovulation_day + timedelta(days=1)
+				print(inner_menu)
+				inner_menu_input = input()
+				match inner_menu_input:
+					case '1':
+						print("Your next period date: ", next_period_date)
+						print("Your estimated ovulation date:", ovulation_day)
+						print("Your fertile window date starts: ", fertile_window_start)
+						print("Your fertile window date ends: ", fertile_window_end)
+					case '2':
+						print("Your next period date: ", next_period_date)
+					case '3':
+						print("Your estimated ovulation date:", ovulation_day)
+					case '4':
+						print("Your estimated Safe date:", ovulation_day)
+					case '5':
+						print("Your fertile window date starts: ", fertile_window_start)
+						print("Your fertile window date ends: ", fertile_window_end)
+					case '0':
+						exit1 = False
 						
 		case '0':
 			exit = False
--- Python file/testMenstrualcycleapp.py
+++ Python file/testMenstrualcycleapp.py
@@ -1,43 +1,30 @@
-from datetime import datetime, timedelta
+#from datetime import datetime, timedelta
 
-import menstrualcycleapp.py
+import menstrualcycleapp
 
 from unittest import TestCase
 
 class TestMenstrualcycleapp(TestCase):
 
 	def test_that_year_is_valid(self):
-		response = menstrualcycleapp.valid_year(2026)
+		response = menstrualcycleapp.date_validation(2026)
 		self.assertEqual(response, "Invalid year")
 		
 	def test_that_month_is_valid(self):
-		response = menstrualcycleapp.valid_month(13)
+		response = menstrualcycleapp.date_validation(13)
 		self.assertEqual(response, "Invalid month")
 
-
 	def test_that_day_is_valid(self):
-		response = menstrualcycleapp.valid_day(11)
+		response = menstrualcycleapp.date_validation(0)
 		self.assertEqual(response, "Invalid day")
 
 	def test_that_first_day_function_works(self):
-		response = menstrualcycleapp.first_day(2025, 5, 11)
+		year,month,day = 2025, 5, 11
+		response = menstrualcycleapp.first_day(year,month,day)
 		self.assertEqual(response, "2025, 5, 11")
-		
-	def test_that_last_day_function_works(self):
-		response = menstrualcycleapp.last_day(2025, 5, 12)
-		self.assertEqual(response, "2025, 5, 11")
-	
+			
 	def test_that_calculate_cycle_length_works(self):
 		response = menstrualcycleapp.calculate_cycle_length(2025, 5, 11, 2025, 5, 12)
-		self.assertEqual(response, "valid input")
-
-	
-	#def test_that_answer_list_function_works(self):
-		response = menstrualcycleapp.answer_list([1][1])
-		self.assertEqual(response, "lagos")
-
-	#def test_that_question_list_function_exists(self):
-		response = menstrualcycleapp.first_day(10)
 		self.assertEqual(response, "valid input")
 
 
