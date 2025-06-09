2eaa191354c9c01b835b643cb5902bf98124b5c3
Macwinner1 <ifechukwuw@gmail.com>
2025-06-09 10:53:41 +0100

    working but still want to refactor it

--- JavaScript file/CreditCardValidator.js
+++ JavaScript file/CreditCardValidator.js
@@ -1,4 +1,8 @@
-let cardNumber = [4,3,8,8,5,7,6,0,1,8,4,1,0,7,0,7];
+//let cardNumber = [4,3,8,8,5,7,6,0,1,8,4,0,2,6,2,6];
+//let cardNumber = [4,3,8,8,5,7,6,0,1,8,4,1,0,7,0,7];
+let cardNumber = [5,3,9,9,8,3,1,6,1,9,6,9,0,4,0,3];
+
+
 
 function AtmCardNumber(cardNumber){
 let cardLength = cardNumber.length;
@@ -15,11 +19,11 @@ valid : "invalid",
 reason : "Invalid length"
 };
 
-for(let count = cardLength; count > 0; count-=2){
+for(let count = 0; count < cardLength; count+=2){
 let checkDigit = cardNumber[count] * 2;
 if(checkDigit >= 10){
 let tempRight = checkDigit % 10;
-let tempLeft = checkDigit / 10;
+let tempLeft = Math.floor(checkDigit / 10);
 checkDigit = tempLeft + tempRight;
 evenIndexDigits += checkDigit;
 }
@@ -28,29 +32,28 @@ evenIndexDigits += checkDigit;
 }
 }
 
-for(let count = cardLength; count > 0; count-=3){
-oddIndexDigits += cardNumber[count];
+for(let counter = 1; counter < cardLength; counter+=2){
+oddIndexDigits += cardNumber[counter];
 }
-
 checkValid = evenIndexDigits + oddIndexDigits;
 
 if(cardLength == 15 && cardNumber[0] == 3 && cardNumber[1] == 7 && checkValid % 10 == 0){
-cardAmericaExpress.valid = valid;
+cardAmericaExpress.valid = "valid";
 cardAmericaExpress.issuer = "America Express";
 return cardAmericaExpress;
 }
-if(cardLength == 16 && cardNumber[0] == 4 && checkValid % 10 == 0){
-cardVisa.valid = valid;
+else if(cardLength == 16 && cardNumber[0] == 4 && checkValid % 10 == 0){
+cardVisa.valid = "valid";
 cardVisa.issuer = "Visa";
 return cardVisa;
 }
-if(cardLength == 16 && cardNumber[0] == 5 && checkValid % 10 == 0){
-cardMasterCard.valid = valid;
+else if(cardLength == 16 && cardNumber[0] == 5 && checkValid % 10 == 0){
+cardMasterCard.valid = "valid";
 cardMasterCard.issuer = "MasterCard";
 return cardMasterCard;
 }
-if(cardLength == 16 && cardNumber[0] == 6 && checkValid % 10 == 0){
-cardDiscover.valid = valid;
+else if(cardLength == 16 && cardNumber[0] == 6 && checkValid % 10 == 0){
+cardDiscover.valid = "valid";
 cardDiscover.issuer = "Discover";
 return cardDiscover;
 }
