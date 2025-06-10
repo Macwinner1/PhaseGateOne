6e74f3264ae3abd0be8514c54f4f6c66a71feb4f
Macwinner1 <ifechukwuw@gmail.com>
2025-06-10 08:30:16 +0100

    Changed from taking input as an array to string and return output to string

--- Java file/CreditCardValidation.java
+++ Java file/CreditCardValidation.java
@@ -1,12 +1,17 @@
 import java.util.Arrays;
 public class CreditCardValidation {
 
-    public static String AtmCardNumber(int[] cardNumber) {
+    public static String AtmCardNumber(String Number) {
+    	   String[] cardNumberString = Number.split("");
+    	   int[] cardNumber = new int[cardNumberString.length];
+        for (int value = 0; value < cardNumberString.length; value++) {
+            cardNumber[value] = Integer.parseInt(cardNumberString [value]);
+        }
         int cardLength = cardNumber.length;        
         int evenIndexDigits = 0;
         int oddIndexDigits = 0;
         int checkValid = 0;
-        String cardNumberStr = Arrays.toString(cardNumber).replace("[", "").replace("]", "").replace(",", "");
+
         String cardStatus = " ";
           
         StringBuilder cardTemplate = new StringBuilder();
@@ -34,29 +39,29 @@ cardTemplate.append("***************************************\n")
         checkValid = evenIndexDigits + oddIndexDigits;
         
         if (cardLength == 15 && cardNumber[0] == 3 && cardNumber[1] == 7 && checkValid % 10 == 0) {
-           cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(cardNumberStr)).replace("{cardType}", "American Express").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Valid");
+           cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(Number)).replace("{cardType}", "American Express").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Valid");
         	return cardStatus;
 
         } else if (cardLength == 16 && cardNumber[0] == 4 && checkValid % 10 == 0) {
-           cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(cardNumberStr)).replace("{cardType}", "Visa").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Valid");
+           cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(Number)).replace("{cardType}", "Visa").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Valid");
         	return cardStatus;
         	
         } else if (cardLength == 16 && cardNumber[0] == 5 && checkValid % 10 == 0) {
-        	cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(cardNumberStr)).replace("{cardType}", "MasterCard").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Valid");
+        	cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(Number)).replace("{cardType}", "MasterCard").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Valid");
         	return cardStatus;
         
         } else if (cardLength == 16 && cardNumber[0] == 6 && checkValid % 10 == 0) {
-        	cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(cardNumberStr)).replace("{cardType}", "Discovery").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Valid");
+        	cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(Number)).replace("{cardType}", "Discovery").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Valid");
         	return cardStatus;
         	
         } else {
-        	cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(cardNumberStr)).replace("{cardType}", "Discovery").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Invalid");
+        	cardStatus = cardTemplate.toString().replace("{cardNumber}", String.valueOf(Number)).replace("{cardType}", "Discovery").replace("{cardLength}", String.valueOf(cardLength)).replace("{status}", "Invalid");
         	return cardStatus;
         }
     }
     
     public static void main(String[] args) {
-        int[] cardNumber = {5, 3, 9, 9, 8, 3, 1, 6, 1, 9, 6, 9, 0, 4, 0, 3};
+        String cardNumber = "5399831619690403";
         System.out.println(CreditCardValidation.AtmCardNumber(cardNumber));
     }
 }
